384d6958e98fbcd8e99cf2289d40466ce1d6a0dc
ms895 <ms895@163.com>
2014-12-08 22:47:40 +0800

    update category

--- controller/home/category.php
+++ controller/home/category.php
@@ -1,20 +1,20 @@
 <?php
 
 /*
- *                                                                          
- *       _/_/_/                      _/        _/_/_/_/_/                     
- *    _/          _/_/      _/_/    _/  _/          _/      _/_/      _/_/    
- *   _/  _/_/  _/_/_/_/  _/_/_/_/  _/_/          _/      _/    _/  _/    _/   
- *  _/    _/  _/        _/        _/  _/      _/        _/    _/  _/    _/    
- *   _/_/_/    _/_/_/    _/_/_/  _/    _/  _/_/_/_/_/    _/_/      _/_/       
- *                                                                          
+ *
+ *       _/_/_/                      _/        _/_/_/_/_/
+ *    _/          _/_/      _/_/    _/  _/          _/      _/_/      _/_/
+ *   _/  _/_/  _/_/_/_/  _/_/_/_/  _/_/          _/      _/    _/  _/    _/
+ *  _/    _/  _/        _/        _/  _/      _/        _/    _/  _/    _/
+ *   _/_/_/    _/_/_/    _/_/_/  _/    _/  _/_/_/_/_/    _/_/      _/_/
+ *
  *
  *  Copyright 2013-2014, Geek Zoo Studio
  *  http://www.ecmobile.cn/license.html
  *
  *  HQ China:
- *    2319 Est.Tower Van Palace 
- *    No.2 Guandongdian South Street 
+ *    2319 Est.Tower Van Palace
+ *    No.2 Guandongdian South Street
  *    Beijing , China
  *
  *  U.S. Office:
@@ -37,32 +37,60 @@ if ( !empty($category) ) {
     foreach ( $category as $key => $val ) {
 
         $goods = array();
-        $children = get_children($val['id']); 
+        $children = get_children($val['id']);
         $goods = gz_get_category_recommend_goods('best', $children);
         if ( count( $goods ) > 3 ) { $goods = array_slice($goods, 0, 3); }
 
         $ngoods = array();
         if ( !empty( $goods ) )
-        foreach ( $goods as $k => $v ) {
-            $ngoods[] = array(
-                'id'=>$v['id'],
-                'name'=>$v['name'],
-                'market_price'=>$v['market_price'],  
-                'shop_price'=>$v['shop_price'],    
-                'promote_price'=>$v['promote_price'],    
-                'brief'=>$v['brief'],
-                'img'=> array(
-                    'thumb' => API_DATA('PHOTO', $v['goods_img']),
-                    'url' => API_DATA('PHOTO', $v['original_img']),
-                    'small' => API_DATA('PHOTO', $v['thumb'])
-                )
-            );
+            foreach ( $goods as $k => $v ) {
+
+                $ngoods[] = array(
+                    'id'=>$v['id'],
+                    'name'=>$v['name'],
+                    'market_price'=>$v['market_price'],
+                    'shop_price'=>$v['shop_price'],
+                    'promote_price'=>$v['promote_price'],
+                    'brief'=>$v['brief'],
+                    'img'=> array(
+                        'thumb' => API_DATA('PHOTO', $v['goods_img']),
+                        'url' => API_DATA('PHOTO', $v['original_img']),
+                        'small' => API_DATA('PHOTO', $v['thumb'])
+                    )
+                );
+            }
+
+
+        //$sql = "SELECT `brand_id` FROM ".$GLOBALS['ecs']->table('goods') ." left join  WHERE ecs_goods.`cat_id` = 3  GROUP BY ecs_goods.`brand_id`";
+
+        $sql = "SELECT  b.* FROM ".$GLOBALS['ecs']->table('goods') ."   LEFT JOIN ".
+            $GLOBALS['ecs']->table('brand') ."  AS b ON b.`brand_id` = ".
+            $GLOBALS['ecs']->table('goods') .".`brand_id`  WHERE ".
+            $GLOBALS['ecs']->table('goods') .".`cat_id` = ".$val['id']."  AND  b.`brand_name` IS NOT NULL  GROUP BY ".
+            $GLOBALS['ecs']->table('goods') .".`brand_id` "
+
+        ;
+
+        $brand=[];
+
+        $ret = $GLOBALS['db']->getAll($sql);
+
+
+        foreach ($ret as $key=>$value )
+        {
+
+            $brand_tmp["brandName"] = $value['brand_name'];
+            $brand_tmp["brandId"] = $value['brand_id'];
+            $brand[] = $brand_tmp;
+
         }
 
+
+
         $categoryGoods[] = array(
-            'id'=>$val['id'],
-            'name'=>$val['name'],
-            'goods'=>$ngoods
+            'catId'=>$val['id'],
+            'catName'=>$val['name'],
+            'subBrands'=>$brand
         );
 
 
@@ -79,13 +107,13 @@ function gz_get_category_recommend_goods($type = '', $cats = '', $brand = 0, $mi
     $price_where .= ($max > 0) ? " AND g.shop_price <= $max " : '';
 
     $sql =  'SELECT g.goods_id, g.goods_name, g.goods_name_style, g.market_price, g.shop_price AS org_price, g.promote_price, ' .
-                "IFNULL(mp.user_price, g.shop_price * '$_SESSION[discount]') AS shop_price, ".
-                'promote_start_date, promote_end_date, g.goods_brief, g.goods_thumb, goods_img, g.original_img, b.brand_name ' .
-            'FROM ' . $GLOBALS['ecs']->table('goods') . ' AS g ' .
-            'LEFT JOIN ' . $GLOBALS['ecs']->table('brand') . ' AS b ON b.brand_id = g.brand_id ' .
-            "LEFT JOIN " . $GLOBALS['ecs']->table('member_price') . " AS mp ".
-                    "ON mp.goods_id = g.goods_id AND mp.user_rank = '$_SESSION[user_rank]' ".
-            'WHERE g.is_on_sale = 1 AND g.is_alone_sale = 1 AND g.is_delete = 0 ' . $brand_where . $price_where . $ext;
+        "IFNULL(mp.user_price, g.shop_price * '$_SESSION[discount]') AS shop_price, ".
+        'promote_start_date, promote_end_date, g.goods_brief, g.goods_thumb, goods_img, g.original_img, b.brand_name ' .
+        'FROM ' . $GLOBALS['ecs']->table('goods') . ' AS g ' .
+        'LEFT JOIN ' . $GLOBALS['ecs']->table('brand') . ' AS b ON b.brand_id = g.brand_id ' .
+        "LEFT JOIN " . $GLOBALS['ecs']->table('member_price') . " AS mp ".
+        "ON mp.goods_id = g.goods_id AND mp.user_rank = '$_SESSION[user_rank]' ".
+        'WHERE g.is_on_sale = 1 AND g.is_alone_sale = 1 AND g.is_delete = 0 ' . $brand_where . $price_where . $ext;
     $num = 0;
     $type2lib = array('best'=>'recommend_best', 'new'=>'recommend_new', 'hot'=>'recommend_hot', 'promote'=>'recommend_promotion');
     $num = get_library_number($type2lib[$type]);
@@ -135,7 +163,7 @@ function gz_get_category_recommend_goods($type = '', $cats = '', $brand = 0, $mi
         $goods[$idx]['brief']        = $row['goods_brief'];
         $goods[$idx]['brand_name']   = $row['brand_name'];
         $goods[$idx]['short_name']   = $GLOBALS['_CFG']['goods_name_length'] > 0 ?
-                                       sub_str($row['goods_name'], $GLOBALS['_CFG']['goods_name_length']) : $row['goods_name'];
+            sub_str($row['goods_name'], $GLOBALS['_CFG']['goods_name_length']) : $row['goods_name'];
         $goods[$idx]['market_price'] = price_format($row['market_price']);
         $goods[$idx]['shop_price']   = price_format($row['shop_price']);
         $goods[$idx]['thumb']        = get_image_path($row['goods_id'], $row['goods_thumb'], true);
